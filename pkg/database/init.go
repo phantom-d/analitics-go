c0607f3ba89d4ed2bab61a875c7cea75bda541a1
Anton Ermolovich <a.ermolovich@mail1.pro>
2022-02-02 19:59:14 +0300

    Добавле интерфейс для подключений к разным типам БД Переработано применение миграций Добавлен индекс для проверки актуальности изменений product-price Убраны горутины для обработки отдельных записей воркера (превышено количество подключений к БД) Реализован полный цикл обработки очереди `product-price` TODO: - реализовать запуск демонов в отдельных процессах - реализовать запуск воркеров в отдельных процессах

--- pkg/database/init.go
+++ pkg/database/init.go
@@ -1,11 +1,13 @@
 package database
 
+import "analitics/pkg/database/drivers"
+
 type Factory map[string]func() Connection
 
 var factory = make(Factory)
 
 func init() {
-	factory.Register("clickhouse", func() Connection { return &Clickhouse{} })
+	factory.Register("clickhouse", func() Connection { return &drivers.Clickhouse{} })
 }
 
 func (factory *Factory) Register(name string, factoryFunc func() Connection) {
